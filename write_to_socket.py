d3d369fecf74592e81ff0d5256425072617a03f8
Etsu <etsu4296@gmail.com>
2017-10-22 23:38:12 +0300

    Create write_to_socket.py

---
+++ write_to_socket.py
@@ -0,0 +1,7 @@
+def mysend(sock, msg):
+    totalsent = 0
+    while totalsent < len(msg):
+        sent = sock.send(msg[totalsent:])  # начиная со смещения totalsent
+        if sent == 0:
+            raise RuntimeError("broken")
+        totalsent = totalsent + sent
