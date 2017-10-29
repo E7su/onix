b9cce8cb40df7e4da00d0e867926b3315f1418da
Etsu <etsu4296@gmail.com>
2017-10-29 21:31:15 +0300

    fix wsgi init.sg

--- wsgi/init.sh
+++ wsgi/init.sh
@@ -7,17 +7,18 @@ mkdir -p /home/box/web/public/css
 mkdir -p /home/box/web/public/js
 mkdir -p /home/box/web/uploads
 mkdir -p /home/box/web/etc
+mkdir -p /home/box/etc
 
 # copy nginx config
-cp /home/box/onix/nginx/nginx.conf  /home/box/web/etc/nginx.conf
+cp /home/box/onix/wsgi/nginx.conf  /home/box/web/etc/nginx.conf
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
 # copy gunicorn config
 cp gunicorn.conf /home/box/etc/
-ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/hello.py
+sudo ln -s /home/box/etc/gunicorn.conf /etc/gunicorn.d/test
 
 # copy wsgi application
 cp hello.py /home/box/web/
 
-
+sudo /etc/init.d/gunicorn restart
