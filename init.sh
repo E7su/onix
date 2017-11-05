88d714ae29e376e25ebecfafd4c27a9718a9215e
Etsu <etsu4296@gmail.com>
2017-11-05 20:20:56 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sudo /etc/init.d/nginx start
+ps -o pid,euser,egroup,comm,args -C nginx
+  
+mkdir -p /home/box/web/public/img
+mkdir -p /home/box/web/public/css
+mkdir -p /home/box/web/public/js
+mkdir -p /home/box/web/uploads
+mkdir -p /home/box/web/etc
+mkdir -p /home/box/etc
+
 # copy nginx config                                                             
 cp etc/nginx.conf  /home/box/web/etc/nginx.conf                                 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default      
