bafa24beab937bc7979670950b76a6aa116efbc5
Etsu <etsu4296@gmail.com>
2017-11-05 22:38:19 +0300

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -39,9 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
-MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'django.contrib.sessions.middleware.SessionMiddleware',
+MIDDLEWARE = [ 
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
