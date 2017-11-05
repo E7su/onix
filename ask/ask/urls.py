b18842681b078965cb3602be21383211a866e8a6
Etsu <etsu4296@gmail.com>
2017-11-05 23:29:43 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -3,11 +3,11 @@ from django.contrib import admin
 admin.autodiscover()                                                            
                                                                                 
 urlpatterns = [                                                                 
-   url(r'^$', include('ask.qa.views') ),                   
-   url(r'^login/.*$', include('ask.qa.views'), name='login'),                  
-   url(r'^signup/.*', include('ask.qa.views'), name='signup'),
-   url(r'^question/(?P<id>[0-9]+)/$', include('ask.qa.views'), name='question'),                        
-   url(r'^ask/.*', include('ask.qa.views'), name='ask'),                                                                             
-   url(r'^popular/.*', include('ask.qa.views'), name='popular'),
-   url(r'^new/.*', include('ask.qa.views'), name='new'),                                                                                                  
+   url(r'^$', include('ask.qa.urls') ),                   
+   url(r'^login/.*$', include('ask.qa.urls'), name='login'),                  
+   url(r'^signup/.*', include('ask.qa.urls'), name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', include('ask.qa.urls'), name='question'),                        
+   url(r'^ask/.*', include('ask.qa.urls'), name='ask'),                                                                             
+   url(r'^popular/.*', include('ask.qa.urls'), name='popular'),
+   url(r'^new/.*', include('ask.qa.urls'), name='new'),                                                                                                  
 ]     
